e0025486c19db7874d626904b0273a17f256d3dd
Karabo Masalesa <kaaribu@protonmail.com>
2023-01-25 23:38:14 +0200

    Added docstring for TicketAdmin class

--- my_band/admin.py
+++ my_band/admin.py
@@ -23,6 +23,16 @@ admin.site.register(Image, ImagesAdmin)
 
 
 class TicketAdmin(admin.ModelAdmin):
+    """Admin class for the Ticket model
+
+         This class is used to display the Ticket model in the admin interface
+
+            Attributes:
+                date_hierarchy (str): A string that is the name of the field to use for the date hierarchy
+                list_filter (tuple): A tuple of strings that are the names of the fields to use for filtering
+                list_display (tuple): A tuple of strings that are the names of the fields to display in the admin
+                search_fields (tuple): A tuple of strings that are the names of the fields to use for searching
+    """
     date_hierarchy = 'created_at'
     list_filter = ('status', 'assignee')
     list_display = ('id', 'title', 'assignee', 'status', 'description', 'updated_at')
