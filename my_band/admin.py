8e702ee8d87aa695ac0dc0f77090d1c55ebc1809
Karabo Masalesa <kaaribu@protonmail.com>
2023-01-25 23:37:50 +0200

    Added docstring for ImageAdmin class

--- my_band/admin.py
+++ my_band/admin.py
@@ -8,14 +8,25 @@ admin.site.register(Order)
 
 
 class ImagesAdmin(admin.ModelAdmin):
+    """Admin class for the Image model
+
+       This class is used to display the Image model in the admin interface
+
+       Attributes:
+              list_display (tuple): A tuple of strings that are the names of the fields to display in the admin
+              interface. The fields are displayed in the order they are listed in the tuple.
+    """
     list_display = ('album', 'image_file')
 
+
 admin.site.register(Image, ImagesAdmin)
+
 
 class TicketAdmin(admin.ModelAdmin):
     date_hierarchy = 'created_at'
     list_filter = ('status', 'assignee')
     list_display = ('id', 'title', 'assignee', 'status', 'description', 'updated_at')
     search_fields = ('title', 'status')
+
 
 admin.site.register(Ticket, TicketAdmin)
