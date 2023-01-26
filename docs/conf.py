dfeced7ce9ff4504b421df0b12fc92b44582164b
Karabo Masalesa <kaaribu@protonmail.com>
2023-01-26 14:08:46 +0200

    Updated conf.py file

--- docs/conf.py
+++ docs/conf.py
@@ -11,6 +11,7 @@ import django
 
 sys.path.insert(0, os.path.abspath('..'))
 os.environ['DJANGO_SETTINGS_MODULE'] = 'my_band.settings'
+django.setup()
 
 project = 'My Band'
 copyright = '2023, Karabo Masalesa'
